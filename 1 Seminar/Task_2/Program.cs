0fe472762b367a612f27e3d32103595b02d69e2a
Lust1 <leva95reva@yahoo.com>
2023-01-13 22:51:35 +0300

    Max number

--- 1 Seminar/Task_2/Program.cs
+++ 1 Seminar/Task_2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber > secondNumber){
+    Console.WriteLine("max = " + firstNumber);
+} else {
+    Console.WriteLine("max = " + secondNumber);
+}
+
