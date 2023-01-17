f5bbc2d9cfa6bf4aa6f1a1ecce6049ef0420f987
Lust1 <leva95reva@yahoo.com>
2023-01-17 22:30:09 +0300

    Add Task_10

---
+++ 2_Seminar/Task_10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int workingNumber = num;
+workingNumber /= 10;
+Console.Write(num + " -> " + workingNumber % 10);
