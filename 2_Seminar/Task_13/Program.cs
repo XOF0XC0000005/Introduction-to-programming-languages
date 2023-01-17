fdb4283c1b5c7632981eec05a0086a22616a0e15
Lust1 <leva95reva@yahoo.com>
2023-01-17 22:46:33 +0300

    Add Task_13

---
+++ 2_Seminar/Task_13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num / 100 == 0)
+{
+    Console.Write("Третьей цифры нет");
+}
+if (num >= 100 && num <= 999)
+{
+    Console.WriteLine(num + " -> " + num % 10);
+}
+if (num >= 1000)
+{
+    int workingNumber = num;
+    workingNumber /= 100;
+    Console.WriteLine(num + " -> " + workingNumber % 10);
+}
