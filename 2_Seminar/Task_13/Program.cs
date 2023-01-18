421a2bc752568bad23de8c1e1e5efb33fd5409f9
Lust1 <leva95reva@yahoo.com>
2023-01-18 11:11:09 +0300

    Additional for Task_13

--- 2_Seminar/Task_13/Program.cs
+++ 2_Seminar/Task_13/Program.cs
@@ -1,15 +1,29 @@
-﻿Console.Write("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int workingNumber = num;
-if (workingNumber / 100 == 0)
+﻿//Усложнение от Алексея
+Console.Write("Введите число: ");
+string stringNum = Console.ReadLine();
+Console.Write("Какую цифру по счёту вам нужно вывести: ");
+int findNum = Convert.ToInt32(Console.ReadLine());
+if (findNum <= stringNum.Length)
 {
-    Console.Write("Третьей цифры нет");
+    Console.WriteLine(stringNum + " -> " + stringNum[findNum - 1]);
 }
-if (workingNumber >= 100)
+else
 {
-    while (workingNumber >= 999)
-    {
-        workingNumber = workingNumber / 10;
-    }
-    Console.WriteLine(num + " -> " + workingNumber % 10);
+    Console.WriteLine("Такого индекса нет в числе");
 }
+//По заданию
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int workingNumber = num;
+// if (workingNumber / 100 == 0)
+// {
+//     Console.Write("Третьей цифры нет");
+// }
+// if (workingNumber >= 100)
+// {
+//     while (workingNumber >= 999)
+//     {
+//         workingNumber = workingNumber / 10;
+//     }
+//     Console.WriteLine(num + " -> " + workingNumber % 10);
+// }
