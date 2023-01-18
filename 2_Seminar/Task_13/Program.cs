0096d9f6c4bc8d50cc7496324a7ee63cb9f7269a
Lust1 <leva95reva@yahoo.com>
2023-01-18 18:46:47 +0300

    Add second additional

--- 2_Seminar/Task_13/Program.cs
+++ 2_Seminar/Task_13/Program.cs
@@ -1,16 +1,33 @@
-﻿//Усложнение от Алексея
+﻿//Усложнение от Алексея 2 вариант
 Console.Write("Введите число: ");
-string stringNum = Console.ReadLine();
+string numString = Console.ReadLine();
+int numLength = numString.Length;
+int num = Convert.ToInt32(numString);
 Console.Write("Какую цифру по счёту вам нужно вывести: ");
 int findNum = Convert.ToInt32(Console.ReadLine());
-if (findNum <= stringNum.Length)
-{
-    Console.WriteLine(stringNum + " -> " + stringNum[findNum - 1]);
+if(findNum > numLength || findNum < 0){
+    Console.WriteLine("Таких индексов нет в этом числе");
+    return;
 }
-else
-{
-    Console.WriteLine("Такого индекса нет в числе");
+int index = numLength - findNum;
+for(int i = 0; i < index; i++){
+    num /= 10;
 }
+Console.WriteLine(numString + " -> " + num % 10);
+
+//Усложнение от Алексея 1 вариант
+// Console.Write("Введите число: ");
+// string stringNum = Console.ReadLine();
+// Console.Write("Какую цифру по счёту вам нужно вывести: ");
+// int findNum = Convert.ToInt32(Console.ReadLine());
+// if (findNum <= stringNum.Length)
+// {
+//     Console.WriteLine(stringNum + " -> " + stringNum[findNum - 1]);
+// }
+// else
+// {
+//     Console.WriteLine("Такого индекса нет в числе");
+// }
 //По заданию
 // Console.Write("Введите число: ");
 // int num = Convert.ToInt32(Console.ReadLine());
