ecb201093cd895aadcdcbeca4236e67c10864a39
Lust1 <leva95reva@yahoo.com>
2023-01-18 09:59:33 +0300

    Fixing Task_15

--- 2_Seminar/Task_15/Program.cs
+++ 2_Seminar/Task_15/Program.cs
@@ -2,53 +2,18 @@
 Console.Write("Введите день недели цифрой: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-MondayCheck(num);
-TuesdayCheck(num);
-WednesdayCheck(num);
-ThursdayCheck(num);
-FridayCheck(num);
-SaturdayCheck(num);
-SundayCheck(num);
+WeekendCheck(num);
 
-void MondayCheck(int number){
-    if(number == 1)
+void WeekendCheck(int number)
+{
+    if (number <= 5 &&  number >= 1)
     {
         Console.WriteLine(number + " -> нет");
     }
-}
-void TuesdayCheck(int number){
-    if(number == 2)
-    {
-        Console.WriteLine(number + " -> нет");
-    }
-}
-void WednesdayCheck(int number){
-    if(number == 3)
-    {
-        Console.WriteLine(number + " -> нет");
-    }
-}
-void ThursdayCheck(int number){
-    if(number == 4)
-    {
-        Console.WriteLine(number + " -> нет");
-    }
-}
-void FridayCheck(int number){
-    if(number == 5)
-    {
-        Console.WriteLine(number + " -> нет");
-    }
-}
-void SaturdayCheck(int number){
-    if(number == 6)
+    else if (number == 6 || number == 7)
     {
         Console.WriteLine(number + " -> да");
-    }
-}
-void SundayCheck(int number){
-    if(number == 7)
-    {
-        Console.WriteLine(number + " -> да");
+    } else {
+        Console.WriteLine("Введите день недели!");
     }
 }
