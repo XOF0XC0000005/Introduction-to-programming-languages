2904a7b988e26a4709af19d3028af6a53c8391de
Lust1 <leva95reva@yahoo.com>
2023-01-20 22:32:16 +0300

    Add fix to Task_21

--- 3_Seminar/Task_21/Program.cs
+++ 3_Seminar/Task_21/Program.cs
@@ -6,5 +6,3 @@ var aArray = aCoordinates.Split(" ").Select(double.Parse).ToArray();
 var bArray = bCoordinates.Split(" ").Select(double.Parse).ToArray();
 double result = Math.Round(Math.Sqrt(Math.Pow(aArray[0] - bArray[0], 2) + Math.Pow(aArray[1] - bArray[1], 2) + Math.Pow(aArray[2] - bArray[2], 2)), 3);
 Console.Write($"A ({aArray[0]}, {aArray[1]}, {aArray[2]}); B({bArray[0]}, {bArray[1]}, {bArray[2]}) -> " + result);
-
-
