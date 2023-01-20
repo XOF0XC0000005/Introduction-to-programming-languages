7e5477b31355eaba5f9609d816a8f3383aa1b597
Lust1 <leva95reva@yahoo.com>
2023-01-20 21:58:43 +0300

    Add Task_19

---
+++ 3_Seminar/Task_19/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int resultNum = num;
+string reverseNum = "";
+while(num != 0)
+{
+    reverseNum += num % 10;
+    num /= 10;
+}
+Console.Write(resultNum + " -> ");
+if(resultNum == Convert.ToInt32(reverseNum)) Console.Write("Да");
+else Console.Write("Нет");
