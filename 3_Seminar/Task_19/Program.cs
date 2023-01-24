368b469fedb127111d4231a08e8f2de9a837df5a
Lust1 <leva95reva@yahoo.com>
2023-01-24 10:29:34 +0300

    add additional

--- 3_Seminar/Task_19/Program.cs
+++ 3_Seminar/Task_19/Program.cs
@@ -1,12 +1,28 @@
-﻿Console.Write("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int resultNum = num;
-string reverseNum = "";
-while(num != 0)
-{
+﻿ Console.Write("Введите число: ");
+ int num = Convert.ToInt32(Console.ReadLine());
+ int resultNum = num;
+ int reverseNum = num % 10;
+ num /= 10;
+ while(num != 0)
+ {
+    reverseNum *= 10;
     reverseNum += num % 10;
     num /= 10;
-}
-Console.Write(resultNum + " -> ");
-if(resultNum == Convert.ToInt32(reverseNum)) Console.Write("Да");
-else Console.Write("Нет");
+ }
+ Console.Write(resultNum + " -> ");
+ if(resultNum == reverseNum) Console.Write("Да");
+ else Console.Write("Нет");
+
+////Задание Task_19
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int resultNum = num;
+// string reverseNum = "";
+// while(num != 0)
+// {
+//     reverseNum += num % 10;
+//     num /= 10;
+// }
+// Console.Write(resultNum + " -> ");
+// if(resultNum == Convert.ToInt32(reverseNum)) Console.Write("Да");
+// else Console.Write("Нет");
