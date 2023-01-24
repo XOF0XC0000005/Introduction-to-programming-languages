0f4b7e6de16f843bd9448505cf96ec6ac61e768c
Lust1 <leva95reva@yahoo.com>
2023-01-24 22:46:07 +0300

    Add Task_27

--- 4_Seminar/Task_27/Program.cs
+++ 4_Seminar/Task_27/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{num} -> {DigitsSum(num)}");
+
+int DigitsSum(int num)
+{
+    int result = 0;
+    while (num != 0)
+    {
+        result += num % 10;
+        num /= 10;
+    }
+    return result;
+}
