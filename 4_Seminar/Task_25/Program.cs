1974a3b81df246e100ae269fb17b8336899f8291
Lust1 <leva95reva@yahoo.com>
2023-01-24 22:37:15 +0300

    Add Task_25

---
+++ 4_Seminar/Task_25/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число А: ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(CustomPow(firstNum, secondNum));
+
+int CustomPow(int num, int power)
+{
+    int result = num;
+
+    for (int i = 1; i < power; i++)
+    {
+        result *= num;
+    }
+
+    return result;
+}
