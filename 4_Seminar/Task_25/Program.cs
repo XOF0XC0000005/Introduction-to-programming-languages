c21c59d82f6604ecd7d0b8cdb7f28d3dad2bc787
Lust1 <leva95reva@yahoo.com>
2023-01-25 12:05:32 +0300

    Add additional to Task_25

--- 4_Seminar/Task_25/Program.cs
+++ 4_Seminar/Task_25/Program.cs
@@ -1,18 +1,43 @@
-﻿Console.Write("Введите число А: ");
+﻿//Усложнение 
+// 3, 5 = 3 * 3 * 3 * 3 * 3 = (3 + 3 + 3) = (9 + 9 + 9) = (27 + 27 + 27) = (81 + 81 + 81)
+Console.Write("Введите число А: ");
 int firstNum = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число B: ");
 int secondNum = Convert.ToInt32(Console.ReadLine());
 
-Console.Write($"{firstNum}, {secondNum} -> {CustomPow(firstNum, secondNum)}");
+Console.Write($"{firstNum}, {secondNum} -> {CustomPowWithoutMultiply(firstNum, secondNum)}");
 
-int CustomPow(int num, int power)
+int CustomPowWithoutMultiply(int num, int pow)
 {
+    int count = num;
     int result = num;
 
-    for (int i = 1; i < power; i++)
+    for (int i = 1; i < pow; i++)
     {
-        result *= num;
+        for (int j = 1; j < count; j++)
+        {
+            result += num;
+        }
+        num = result;
     }
-
-    return result;
+    return num;
 }
+//Домашка
+// Console.Write("Введите число А: ");
+// int firstNum = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число B: ");
+// int secondNum = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write($"{firstNum}, {secondNum} -> {CustomPow(firstNum, secondNum)}");
+
+// int CustomPow(int num, int power)
+// {
+//     int result = num;
+
+//     for (int i = 1; i < power; i++)
+//     {
+//         result *= num;
+//     }
+
+//     return result;
+// }
