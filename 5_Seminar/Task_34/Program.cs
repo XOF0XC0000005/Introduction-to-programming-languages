7d6559d4cc097788aa0913950e3fabc211cabafa
Lust1 <leva95reva@yahoo.com>
2023-01-27 22:53:10 +0300

    Add Task_34

---
+++ 5_Seminar/Task_34/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+Console.Write("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[length];
+FillArray(array);
+Console.Write("[");
+Console.Write(string.Join(", ", array));
+Console.Write("]");
+Console.Write($" -> {FindAllEvenNumbers(array)}");
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+    }
+}
+
+int FindAllEvenNumbers(int[] array)
+{
+    int counter = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0) counter++;
+    }
+    return counter;
+}
