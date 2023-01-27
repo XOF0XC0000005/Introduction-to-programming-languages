221827c8815ec0be39d1eac891e673c3a2f68284
Lust1 <leva95reva@yahoo.com>
2023-01-27 22:58:51 +0300

    Add Task_36

---
+++ 5_Seminar/Task_36/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Write("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[length];
+FillArray(array);
+Console.Write("[");
+Console.Write(string.Join(", ", array));
+Console.Write("]");
+Console.Write($" -> {FindSummaryNotEvenNumbers(array)}");
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 101);
+    }
+}
+
+int FindSummaryNotEvenNumbers(int[] arr)
+{
+    int sum = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0) sum += arr[i];
+    }
+    return sum;
+}
