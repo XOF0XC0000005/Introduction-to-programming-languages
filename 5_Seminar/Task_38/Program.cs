93d83b4a1eb33d89718b6926ecb8c7d7509f9181
Lust1 <leva95reva@yahoo.com>
2023-01-27 23:04:14 +0300

    Add Task_38

---
+++ 5_Seminar/Task_38/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 38: Задайте массив целых чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+Console.Write("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[length];
+FillArray(array);
+Console.Write("[");
+Console.Write(string.Join(", ", array));
+Console.Write("]");
+Console.Write($" -> {SubstractMaxMin(array)}");
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-10, 101);
+    }
+}
+
+int SubstractMaxMin(int[] arr)
+{
+    int max = arr[0];
+    int min = arr[1];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(max < arr[i]) max = arr[i];
+        if(min > arr[i]) min = arr[i];
+    }
+    return max - min;
+}
+
